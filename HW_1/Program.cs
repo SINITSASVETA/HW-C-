5e2d21b3be827ab1119b208fa87b0df095cfd26e
SinitsaSveta <sinitsa9111@gmail.com>
2022-03-02 22:31:34 +0300

    HW1

---
+++ HW_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Найти максимальное и минимальное числа");
+int a = 5;
+int b = 7;
+int max = b;
+int min = a;
+if (a < max) min = a;
+if (b > min) max = b;
+Console.WriteLine(min);
+Console.WriteLine(max);

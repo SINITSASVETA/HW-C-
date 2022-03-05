5ee901114e7453f11f25ab674ccff54b52274e0b
SinitsaSveta <sinitsa9111@gmail.com>
2022-03-05 10:51:03 +0300

    первая задача решена

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SecondDigitNum(int num1)
+{
+    int num2 =(num1/10);
+    num2 = num2%10;
+    return num2;
+}
+
+
+Console.WriteLine(SecondDigitNum(num));
